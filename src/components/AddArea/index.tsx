f9e5b098567067a8dc25ed5f7bb4dc2ebd6edde3
Matheus Monteiro <matheus_wellyso2015@hotmail.com>
2023-03-20 06:21:28 -0300

    placeholder changed

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -20,7 +20,7 @@ export const AddArea = ({onEnter}: Props) => {
 			<div className="image">➕</div>	
 			<input 
 				type="text"
-				placeholder="Adicione uma tarefa"
+				placeholder="Adicione uma tarefa:"
 				value={inputText}
 				onChange={e=>setInputText(e.target.value)}
 				onKeyUp={handleKeyUp}
